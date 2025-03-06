377b694a8701049fdd763491ae4808115bd7b40a
Johnatas michaell <johnatasmichael067@gmail.com>
2025-03-06 11:06:29 -0300

    aparencia

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,59 @@ class TelaLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.person),
-                    hintText: 'Login',
-                  ),
-                )
+      body: Column(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              color: Colors.blue,
+              child: Center(
+                child: Text(
+                  'Bem vindo',
+                    style:TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold
                 ),
-            ],
+               ),
+              ),
+            ),
           ),
-          ),
-      
+
+          Expanded(
+            flex: 3,
+            child: Padding(
+              padding:  const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.person),
+                      hintText: 'Login'
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      icon: Icon(Icons.lock),
+                      hintText: 'Senha',
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Login Precionado!');
+                    },
+                    child: Text('Login'),
+                  )
+                ],
+              ),
+            ),
+        )
+        ],
+      ),
     );
   }
 }
-
